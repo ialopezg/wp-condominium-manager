ee7c92170463cb266fc5ce898d0f1fc8e8f4f4e7
Isidro A. López G <isidro.lopezg@live.com>
2019-04-18 18:40:59 -0500

    API Settings for Modular Way to add Admin Menus

--- includes/Pages/Admin.php
+++ includes/Pages/Admin.php
@@ -3,18 +3,29 @@
 namespace WPCMPlugin\Pages;
 
 use WPCMPlugin\Base\BaseController;
+use WPCMPlugin\Api\Settings;
 
 class Admin extends BaseController {
+    public $pages = array();
+    public $settings;
+
+    public function __construct() {
+        $this->settings = new Settings();
+
+        $this->pages = array(
+            array(
+                'page_title' => 'WPCM Plugin',
+                'menu_title' => 'WPCM',
+                'capability' => 'manage_options',
+                'menu_slug' => 'wpcm_plugin',
+                'callback' => function() { echo '<h1>WPCM Plugin</h1>'; },
+                'icon_url' => 'dashicons-building',
+                'position' => 110
+            )
+        );
+    }
+
     public function register() {
-        // Admin menu
-        add_action('admin_menu', array($this, 'add_admin_pages'));
-    }
-
-    public function add_admin_pages() {
-        add_menu_page('WPCM Plugin', 'WPCMP', 'manage_options', 'wpcm_plugin', array($this, 'admin_index'), 'dashicons-building', 110);
-    }
-
-    public function admin_index() {
-        require_once($this->plugin_path . 'templates/admin.php');
+        $this->settings->addPages($this->pages)->register();
     }
 }
