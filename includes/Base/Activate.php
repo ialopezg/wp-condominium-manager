31e2376bd88742a5b5251af930fffea0275d4d44
Isidro A. López G <isidro.lopezg@live.com>
2019-05-06 17:50:11 -0500

    Save default options on Activate

--- includes/Base/Activate.php
+++ includes/Base/Activate.php
@@ -10,5 +10,13 @@ class Activate {
     public static function activate() {
         // Flush rewrite rules
         flush_rewrite_rules();
+
+        if (get_option('wpcm_plugin')) {
+            return;
+        }
+
+        $defaults = array();
+
+        update_option('wpcm_plugin', $defaults);
     }
 }
